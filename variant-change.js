f2a29d06530ac32d28d9328b68129312f38540bd
Eric Ruiz Molero <154838631+ericruizmolero@users.noreply.github.com>
2026-04-01 18:48:25 +0200

    Disable markers in variant-change.js

--- variant-change.js
+++ variant-change.js
@@ -71,7 +71,7 @@ window.addEventListener("load", () => {
       onLeaveBack:   () => { darkModeOverrides--; applyVariant(); },
       // Esto ayuda a evitar saltos si el usuario scrollea muy rápido
       fastScrollEnd: true, 
-      markers: true
+      markers: false
     });
   });
 
